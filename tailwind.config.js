3cc3cfb9ab6cd31a32e861d4bf45a7852e8e93a5
Yubo-Cao <cao2006721@gmail.com>
2024-09-16 23:51:00 -0400

    feat: stick with the primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         white: "#FFFFFF",
         black: "#121723",
         dark: "#1D2430",
-        primary: "#4A6CF7",
+        primary: "#3abef9",
         yellow: "#FBB040",
         "bg-color-dark": "#171C28",
         "body-color": {
